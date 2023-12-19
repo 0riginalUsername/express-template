f32786b5e9cd0e29fec55d6c3d51e2f782a02b7d
0riginalUsername <email@example.com>
2023-12-18 18:13:46 -0700

    full-express-implmentaion-with-ws

---
+++ Components/OpenConnection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function OpenCon(){
+
+    return(
+        <>
+        <h1>Test:</h1>
+    <button id="ws-open">
+        Open Connection
+    </button>
+    <button id="ws-close">
+        Close Connection
+    </button>
+        </>
+    )
+    
+}
